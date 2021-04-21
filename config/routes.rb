ca9f97034cc7a7933d6650c0a1a1177e69c2c0d9
staskebooth <jstaske@chicagobooth.edu>
2021-04-21 03:08:23 +0000

    added in the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root "photos#index"
 
+  get "/users/:id" => "users#show", as: :user
+
+
+
   resources :likes
   resources :follow_requests
   resources :comments
